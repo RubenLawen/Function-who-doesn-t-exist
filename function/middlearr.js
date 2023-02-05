2a52bb77eae976c2e3d8bb291213f8aa718a465b
Dimitry <reply.kunah@gmail.com>
2023-02-05 23:34:37 +0100

    Update middlearr.js

--- function/middlearr.js
+++ function/middlearr.js
@@ -1,4 +1,3 @@
 function middlearray(arr) {  
-    if(arr.length % 2 == 0) console.log(arr[arr.length/2-1])
-    else console.log(arr[arr.length/2-0.5])
+    return (arr[parseInt(arr.length / 2)])
 }
